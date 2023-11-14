aeb009de69bae03486895e859740b0b1c6c19aff
SalmabattTechCampus <salma-albattashi@hotmail.com>
2023-11-14 15:06:39 +0400

    objec

---
+++ object.py
@@ -0,0 +1,9 @@
+age = int (input ("please inter your age: "))
+print(2023-age)
+
+
+number = int(input("Entr a number"))
+print ("%04d"% (number))
+
+name = input("please inter your name: ")
+print (name +"%04d"% (age))
