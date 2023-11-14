5f6196e1e50e512ef6be06da310cf53a7242bd76
SalmabattTechCampus <salma-albattashi@hotmail.com>
2023-11-14 12:41:54 +0400

    tilewidth

--- HelloMakeen.py
+++ HelloMakeen.py
@@ -1,3 +1,41 @@
 print ("hello Makeen")
 
 print ("1 2 3")
+
+a=2
+b=3
+a,b=b,a
+print(a,b)
+
+print(8//3)
+print(2/5)
+print(6//7)
+
+n=6
+print(n/9)
+abs(-173)
+
+print (max)
+
+import math
+print(math.sqrt(25))
+
+name= "Ali"
+age = 23
+print ("hello {} , {}" .format(name,age)) 
+print (5+3)
+ 
+ 
+ 
+
+total = 100
+block_size= 5
+tile_width = total // block_size
+print (tile_width)
+black = tile_width // 2
+white = black -1
+gap = tile_width - (black + white)
+print (gap)
+print (black + white)
+
+
---
+++ tilewidth.py
@@ -0,0 +1,11 @@
+total = 100
+block_size= 5
+tile_width = total // block_size
+print (tile_width)
+black = tile_width // 2
+white = black -1
+gap = tile_width - (black + white)
+print (gap)
+print (black + white)
+
+
