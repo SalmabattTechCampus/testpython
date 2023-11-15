7df2002139fb9c2ab19771c5829bce0501f42c56
SalmabattTechCampus <48808710+SalmabattTechCampus@users.noreply.github.com>
2023-11-15 20:07:26 +0400

    Update time.py

--- if.py
+++ if.py
@@ -1,10 +1,19 @@
-h1 = int(input("enter a hour:"))
-m1 = int(input("enter a m:"))
-ampm2 = (input("enter a pm or am:"))
+h1=int(input("enter a hour for first time"))
+m1=int(input("enter a min for first time:"))
+ampm1=input("input am or pm for first time:")
 
-h2 = int(input("enter a hour:"))
-m2 = int(input("enter a m:"))
-ampm2 = (input("enter pm or pm:"))
+h2=int(input("enter an hour for second time:"))
+h2=int(input("enter an min for second time:"))km;
+ampm1=input("input am or pm for first time:")
 
-if 
-
+if(ampm1 == "am" and ampm2 =="pm"):
+  print ("first time is first:""+ str(h1)+"+str(m1)+ampm1)
+elif ((ampm2 == "am" and ampm1 =="pm"):
+ print ("Second time is first:""+ str(h2)+"+str(m2)+ampm1)
+else:
+  if (h1<h2):
+    print ("first time is first"+ str(h1)+":"+str(m1)+ampm1)
+  else:
+    print ("second time is first"+ str(h2)+":"+str(m2)+ampm2)
+  else:
+  print ("second time is first"+ str(h2)+":"+str(m2)+ampm2)
